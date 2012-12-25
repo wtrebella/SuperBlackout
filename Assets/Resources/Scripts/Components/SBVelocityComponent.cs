67c1b7e2910eedb08c32c343bccd06ba459d8d20
Whitaker Trebella <wtrebella@gmail.com>
2012-12-25 01:51:50 -0500

    made movement a lot more efficient

--- Assets/Resources/Scripts/Components/SBVelocityComponent.cs
+++ Assets/Resources/Scripts/Components/SBVelocityComponent.cs
@@ -40,19 +40,15 @@ public class SBVelocityComponent : SBAbstractComponent {
 			switch (accelerationDirection) {
 			case Direction.Left:
 				xVelocity -= accelAmt;
-				if (xVelocity < -SBConfig.DRINKER_MAX_VELOCITY) xVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
 				break;
 			case Direction.Right:
 				xVelocity += accelAmt;
-				if (xVelocity > SBConfig.DRINKER_MAX_VELOCITY) xVelocity = SBConfig.DRINKER_MAX_VELOCITY;
 				break;
 			case Direction.Down:
 				yVelocity -= accelAmt;
-				if (yVelocity < -SBConfig.DRINKER_MAX_VELOCITY) yVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
 				break;
 			case Direction.Up:
 				yVelocity += accelAmt;
-				if (yVelocity > SBConfig.DRINKER_MAX_VELOCITY) yVelocity = SBConfig.DRINKER_MAX_VELOCITY;
 				break;
 			case Direction.None:
 				break;
@@ -63,24 +59,30 @@ public class SBVelocityComponent : SBAbstractComponent {
 			switch (accelerationDirection) {
 			case Direction.Left:
 				xVelocity -= accelAmt;
-				if (xVelocity < -SBConfig.DRINKER_MAX_VELOCITY) xVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
+				yVelocity -= accelAmt / 2f;
 				break;
 			case Direction.Right:
 				xVelocity += accelAmt;
-				if (xVelocity > SBConfig.DRINKER_MAX_VELOCITY) xVelocity = SBConfig.DRINKER_MAX_VELOCITY;
+				yVelocity += accelAmt / 2f;
 				break;
 			case Direction.Down:
 				yVelocity -= accelAmt;
-				if (yVelocity < -SBConfig.DRINKER_MAX_VELOCITY) yVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
+				xVelocity -= accelAmt / 2f;
 				break;
 			case Direction.Up:
 				yVelocity += accelAmt;
-				if (yVelocity > SBConfig.DRINKER_MAX_VELOCITY) yVelocity = SBConfig.DRINKER_MAX_VELOCITY;
+				xVelocity += accelAmt / 2f;
 				break;
 			case Direction.None:
 				break;
 			}	
 		}
+		
+		if (xVelocity < -SBConfig.DRINKER_MAX_VELOCITY) xVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
+		if (xVelocity > SBConfig.DRINKER_MAX_VELOCITY) xVelocity = SBConfig.DRINKER_MAX_VELOCITY;
+		if (yVelocity < -SBConfig.DRINKER_MAX_VELOCITY) yVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
+		if (yVelocity > SBConfig.DRINKER_MAX_VELOCITY) yVelocity = SBConfig.DRINKER_MAX_VELOCITY;
+
 	}
 	
 	static int frameCount = 0;
@@ -88,35 +90,19 @@ public class SBVelocityComponent : SBAbstractComponent {
 	public void UpdateDeceleration() {
 		float decelAmt = Time.fixedDeltaTime * SBConfig.DRINKER_ACCELERATION_CONSTANT;
 		
-		if (xVelocity > 0 && accelerationDirection != Direction.Right) {
-			xVelocity -= decelAmt;
-			if (xVelocity < 0) xVelocity = 0;
-		}
-		else if (xVelocity < 0 && accelerationDirection != Direction.Left) {
-			xVelocity += decelAmt;
-			if (xVelocity > 0) xVelocity = 0;
-		}
+		float xVelPrev = xVelocity;
+		float yVelPrev = yVelocity;
 		
-		if (yVelocity > 0 && accelerationDirection != Direction.Up) {
-			yVelocity -= decelAmt;
-			if (yVelocity < 0) yVelocity = 0;
-		}
-		else if (yVelocity < 0 && accelerationDirection != Direction.Down) {
-			yVelocity += decelAmt;
-			if (yVelocity > 0) yVelocity = 0;
-		}
+		if (xVelPrev != 0) xVelocity += decelAmt * Mathf.Sign(-xVelocity);
+		if (yVelPrev != 0) yVelocity += decelAmt * Mathf.Sign(-yVelocity);
+		
+		if (Mathf.Sign(xVelPrev) != Mathf.Sign(xVelocity)) xVelocity = 0;
+		if (Mathf.Sign(yVelPrev) != Mathf.Sign(yVelocity)) yVelocity = 0;
+		
+		if (accelerationDirection == Direction.Left || accelerationDirection == Direction.Right) xVelocity = xVelPrev;
+		if (accelerationDirection == Direction.Down || accelerationDirection == Direction.Up) yVelocity = yVelPrev;
 		
 		frameCount++;
-		
-		if (frameCount % 15 == 0 && OwnerDrinkCount() > 0) {
-			xVelocity += Random.Range(-20000f, 20000f) * Time.fixedDeltaTime;
-			yVelocity += Random.Range(-20000f, 20000f) * Time.fixedDeltaTime;
-			
-			if (xVelocity > SBConfig.DRINKER_MAX_VELOCITY) xVelocity = SBConfig.DRINKER_MAX_VELOCITY;
-			if (yVelocity > SBConfig.DRINKER_MAX_VELOCITY) yVelocity = SBConfig.DRINKER_MAX_VELOCITY;
-			if (xVelocity < -SBConfig.DRINKER_MAX_VELOCITY) xVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
-			if (yVelocity < -SBConfig.DRINKER_MAX_VELOCITY) yVelocity = -SBConfig.DRINKER_MAX_VELOCITY;
-		}
 	}
 	
 	private int OwnerDrinkCount() {
